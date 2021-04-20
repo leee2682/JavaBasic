edda844fcee370616a4dd4166b48009f11767ed6
leee2682 <78770258+leee2682@users.noreply.github.com>
2021-04-20 17:34:43 +0900

    Add files via upload

---
+++ 20210420/FlowEx23.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class FlowEx23 {
+    public static void main(String[] args) {
+        int menu = 0;
+        int num = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("(1) square");
+            System.out.println("(2) square root");
+            System.out.println("(3) log");
+        } 
+    }
+}
