6dceb3cff6cf09ee16168f82bcf0496a18dbe7e0
leee2682 <78770258+leee2682@users.noreply.github.com>
2021-05-11 17:32:58 +0900

    Add files via upload

---
+++ exam_Security/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public @ResponseBody String root() throws Exception{
+        return "LoginForm(1)";
+    }
+
+    @RequestMapping("/guest/welcome")
+    public String welcome1() {
+        return "guest/welcome1";
+    }
+    
+    @RequestMapping("/member/welcome")
+    public String welcome2() {
+        return "member/welcome2";
+    }
+    
+    @RequestMapping("/admin/welcome")
+    public String welcome3() {
+        return "admin/welcome3";
+    }
+
+    @RequestMapping("/loginForm")
+    public String loginForm() {
+        return "security/loginForm";
+    }
+    
+    @RequestMapping("/loginError")
+    public String loginError() {
+        return "security/loginError";
+    }
+}
+
